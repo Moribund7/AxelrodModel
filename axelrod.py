8e811ba631faaf01c5fc37310bf28a4e4c08e4f7
Filip Kowalski <hakujokonto@gmail.com>
2019-03-12 11:59:01 +0100

    Refactor

--- axelrod.py
+++ axelrod.py
@@ -1,77 +1,84 @@
 import igraph
 import numpy as np
 
-def newNeighbourModelA(g,node):
-    t=[]
-    for i in range(0,g.vcount()):#dla kazdego noda 
-        for j in range(g.degree(i)):#dodajemy go tyle razy ile ma polaczen
-            t.append(i)
-    newNeighbour = node
-    while(newNeighbour==node):
-        newNeighbour=np.random.choice(t)
-    return newNeighbour
 
-def getLargestComponentSize(g): 
+def new_neighbour_model_a(g, node):
+    t = []
+    for i in range(0, g.vcount()):  # dla kazdego noda
+        for j in range(g.degree(i)):  # dodajemy go tyle razy ile ma polaczen
+            t.append(i)
+    new_neighbour = node
+    while new_neighbour == node:
+        new_neighbour = np.random.choice(t)
+    return new_neighbour
+
+
+def get_largest_component_size(g):
     return max([np.size(g.components()[i]) for i in range(len(g.components()))])
 
 
-def getNumActiveConnections(g):
-    activeEdges=0
-    F=len(g.vs.attributes())
+def get_num_active_connections(g):
+    active_edges = 0
+    F = len(g.vs.attributes())
     for i in range(g.vcount()):
         for j in g.neighbors(i):
-            flag=False
+            flag = False
             for k in range(F):
-                if(g.vs[i][str(k)]!=g.vs[j][str(k)]):
-                    flag=True
+                if g.vs[i][str(k)] != g.vs[j][str(k)]:
+                    flag = True
                     break
-            if(flag):
-                activeEdges+=1
-    return activeEdges/2 
-        
-def evolve(g,newNeighbourFun):
-    frozen=False
-    F=len(g.vs.attributes())
-    t=0
-    while(not frozen):
-        t+=1
-        if(not (t%10000)):
-            numActiveConnections=getNumActiveConnections(g)
-            print("t= ",t," active: ",numActiveConnections)
-            if(numActiveConnections == 0):
-                frozen=True
-        a = np.random.randint(0,g.vcount())
-        if(g.neighbors(a)==[]):
+            if flag:
+                active_edges += 1
+    return active_edges / 2
+
+
+def evolve(g, new_neighbour_fun):
+    frozen = False
+    F = len(g.vs.attributes())
+    t = 0
+    while not frozen:
+        t += 1
+        if not (t % 10000):
+            numActiveConnections = get_num_active_connections(g)
+            print("t= ", t, " active: ", numActiveConnections)
+            if numActiveConnections == 0:
+                frozen = True
+        a = np.random.randint(0, g.vcount())
+        if not g.neighbors(a):
             continue
         b = np.random.choice(g.neighbors(a))
-        differentTraits=[]
+        differentTraits = []
         for trait in range(F):
-            if(g.vs[a][str(trait)]!=g.vs[b][str(trait)]):
+            if g.vs[a][str(trait)] != g.vs[b][str(trait)]:
                 differentTraits.append(trait)
-        if(len(differentTraits)==0): ### takie same traity - nic się nie dzieje
+        if len(differentTraits) == 0:  # takie same traity - nic się nie dzieje
             continue
-        if(len(differentTraits)==F): ### zupełnie różne traity - rozłączenie
-            newNeighbour=newNeighbourFun(g,a)
-            g.delete_edges((a,b))
-            g.add_edges([(a,newNeighbour)])
-        else:                       ### zmiana cechy
-            if(np.random.rand() > len(differentTraits)/F):
-                traitToChange=np.random.choice(differentTraits)
-                g.vs[a][str(traitToChange)]=g.vs[b][str(traitToChange)]
+        if len(differentTraits) == F:  # zupełnie różne traity - rozłączenie
+            newNeighbour = new_neighbour_fun(g, a)
+            g.delete_edges((a, b))
+            g.add_edges([(a, newNeighbour)])
+        else:  # zmiana cechy
+            if np.random.rand() > len(differentTraits) / F:
+                traitToChange = np.random.choice(differentTraits)
+                g.vs[a][str(traitToChange)] = g.vs[b][str(traitToChange)]
 
 
-## generacja randomowego grafu z randomowym traitami
-nodesNum=500
-F=3 
-q=1000 
-#tworzymy losowy graf z nodeNum*2 polaczeniami 
-g=igraph.Graph.Erdos_Renyi(n=nodesNum,m=nodesNum*2)
+def main():
+    # generacja randomowego grafu z randomowym traitami
+    nodesNum = 500
+    F = 3
+    q = 1000
+    # tworzymy losowy graf z nodeNum*2 polaczeniami
+    g = igraph.Graph.Erdos_Renyi(n=nodesNum, m=nodesNum * 2)
 
-#dla kazdej z F cech dla kazdego noda wybieramy wartosc z zakresu [0,q[ 
-for i in range(F):
-    g.vs[str(i)]=np.random.randint(0,q,nodesNum)#dlaczego jako string?
+    # dla kazdej z F cech dla kazdego noda wybieramy wartosc z zakresu [0,q[
+    for i in range(F):
+        g.vs[str(i)] = np.random.randint(0, q, nodesNum)  # dlaczego jako string?
 
-evolve(g,newNeighbourModelA)
+    evolve(g, new_neighbour_model_a)
 
-print("gobal clustering ",g.transitivity_undirected())
-print("largest component ",getLargestComponentSize(g),getLargestComponentSize(g)/nodesNum)
+    print("global clustering ", g.transitivity_undirected())
+    print("largest component ", get_largest_component_size(g), get_largest_component_size(g) / nodesNum)
+
+
+main()
