10a4a496666eb98ef37beeab88f183cabedc4601
Filip Kowalski <hakujokonto@gmail.com>
2019-04-03 10:41:32 +0200

    Refactor

--- data_visualization.py
+++ data_visualization.py
@@ -19,6 +19,7 @@ def add_data_to_dict_of_variables(data, dict_of_variables):
         for element in data[q]:
             dict_of_variables[element].append(data[q][element])
 
+
 def load_all_json_data():
     dict_of_variables = None
     for filename in glob.glob('*.json'):
@@ -28,18 +29,17 @@ def load_all_json_data():
             dict_of_variables = create_dict_of_variables(data)
         add_data_to_dict_of_variables(data, dict_of_variables)
     return dict_of_variables
+
+
 if __name__ == "__main__":
 
-
-
     dict_of_variables = load_all_json_data()
-
 
     plt.figure()
 
     for element in dict_of_variables:
         if element != "q":
-            plt.plot(dict_of_variables["q"], dict_of_variables[element], 'o', label=element)  # TODO refactor
+            plt.plot(dict_of_variables["q"], dict_of_variables[element], 'o', label=element)
 
     plt.legend()
     plt.xscale('log')
