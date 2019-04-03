755e179a65b99445c1a8c0643b1a262a11f97ac1
Filip Kowalski <hakujokonto@gmail.com>
2019-04-03 10:26:00 +0200

    Fix colors, add dict for data

--- data_visualization.py
+++ data_visualization.py
@@ -5,8 +5,24 @@ with open("data.json", "r") as f:
     data = json.load(f)
 
 plt.figure()
-for q in data:
-    for element in data[q]:
-        plt.scatter(q,data[q][element]) # TODO refactor, dodac poprawnie kolory
 
+dict_of_variables={}
+for q in data:
+    dict_of_variables["q"]=[]
+    for key in data[q].keys():
+        key_str=str(key)
+        dict_of_variables[key_str]=[]
+
+for q in data:
+    dict_of_variables["q"].append(q)
+    for element in data[q]:
+        dict_of_variables[element].append(data[q][element])
+
+
+for element in dict_of_variables:
+    if element != "q":
+        plt.plot(dict_of_variables["q"],dict_of_variables[element],'o',label=element) # TODO refactor
+
+plt.legend()
+plt.xscale('log')
 plt.show()
