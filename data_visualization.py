f53b367a9bb64c837e43f260bda414c44e887847
Filip Kowalski <hakujokonto@gmail.com>
2019-04-16 12:05:36 +0200

    Working plot from pickle

--- data_visualization.py
+++ data_visualization.py
@@ -32,7 +32,7 @@ def load_all_json_data():
         if dict_of_variables == None:
             dict_of_variables = create_dict_of_variables(data)
         add_data_to_dict_of_variables(data, dict_of_variables)
-    dict_of_variables["q"]=[int(i) for i in dict_of_variables["q"]]
+    dict_of_variables["q"] = [int(i) for i in dict_of_variables["q"]]
     return dict_of_variables
 
 
@@ -45,45 +45,61 @@ def create_dict_of_graphs(data):
 def load_all_pickle_data():
     dict_of_graphs = {}
     for filename in glob.glob('*.pickle'):
-        with open(filename,'rb') as f:
+        with open(filename, 'rb') as f:
             data = pickle.load(f)
             for q in data:
                 print(q)
                 if q in dict_of_graphs.keys():
                     dict_of_graphs[q].append[data[q]]
                 else:
-                    dict_of_graphs[q]=data[q]
+                    dict_of_graphs[q] = data[q]
     return dict_of_graphs
 
-def get_data_from_graphs(dict_of_graphs):
-    out_simulation_data={}
-    for q in dict_of_graphs:
-        global_clustering_sum=0
-        local_clustering_sum=0
-        relative_largest_component_sum=0
 
+def get_data_from_graphs(dict_of_graphs):
+    out_simulation_data = {}
+    for q in dict_of_graphs:
+
+########################################################################################################################
+##################     Tutaj można dodawać funkcje które chcemy policzyc na grafach
+
+        global_clustering_sum = 0
+        local_clustering_sum = 0
+        relative_largest_component_sum = 0
+        ##tu dodaj sume =0, służy do uśredniania po kilku przebiegach dla danego q
         for g in dict_of_graphs[q]:
             global_clustering = g.transitivity_undirected()
             local_clustering = get_local_avg_clustering(g)
             relative_largest_component = get_largest_component_size(g) / nodesNum
+            ##tutaj dopisz w jaki sposób policzyć daną wielkość
 
-        global_clustering_sum += global_clustering
-        local_clustering_sum += local_clustering
-        relative_largest_component_sum += relative_largest_component
-
-
+            global_clustering_sum += global_clustering
+            local_clustering_sum += local_clustering
+            relative_largest_component_sum += relative_largest_component
+            ##dodaj do sumy
         out_from_q_simulations = {"global_clustering": global_clustering_sum / n_realizations,
                                   "local_clustering": local_clustering_sum / n_realizations,
                                   "relative_largest_component": relative_largest_component_sum / n_realizations}
+                                   ##dopisz nazwe i wartosc aby dodac do wykresu
+########################################################################################################################
 
         out_simulation_data[str(q)] = out_from_q_simulations
 
     return out_simulation_data
 
-if __name__ == "__main__":
 
+def clean_data(data):
+    dict_of_variables = create_dict_of_variables(data)
+    add_data_to_dict_of_variables(data, dict_of_variables)
+    dict_of_variables["q"] = [int(i) for i in dict_of_variables["q"]]
+    return dict_of_variables
+
+
+if __name__ == "__main__":
+    #wczytuje dane z pliku pickle
     dict_of_graphs = load_all_pickle_data()
-    dict_of_variables=get_data_from_graphs(dict_of_graphs)
+    dict_of_data = get_data_from_graphs(dict_of_graphs)
+    dict_of_variables = clean_data(dict_of_data)
 
     plt.figure()
 
