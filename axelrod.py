d33d02659427d781157d1ebeabe29391a66dfef4
Filip Kowalski <hakujokonto@gmail.com>
2019-06-03 09:24:01 +0200

    Fix model:

--- axelrod.py
+++ axelrod.py
@@ -6,6 +6,10 @@ import igraph
 import json
 import numpy as np
 
+def plot_graph(g):
+    g.vs["label"] = [str(g.vs["0"][i])+str(g.vs['2'][i])+str(g.vs['2'][i]) for i in range(len(g.vs['0']))]
+
+    igraph.plot(g,vertex_size=40)
 
 def new_neighbour_model_a(g, node):
     t = []
@@ -13,7 +17,7 @@ def new_neighbour_model_a(g, node):
         for j in range(g.degree(i)):  # dodajemy go tyle razy ile ma polaczen
             t.append(i)
     new_neighbour = node
-    while new_neighbour == node:
+    while new_neighbour == node or (new_neighbour in g.neighbors(node)):
         new_neighbour = np.random.choice(t)
     return new_neighbour
 
@@ -24,7 +28,7 @@ def get_largest_component_size(g):
 
 def get_num_active_connections(g):
     active_edges = 0
-    F = len(g.vs.attributes())
+    F = len(g.vs.attributes()) if 'label' not in g.vs.attributes() else len(g.vs.attributes())-1 #rysowanie daje label jako atrybut
     for i in range(g.vcount()):
         for j in g.neighbors(i):
             flag = False
@@ -39,15 +43,15 @@ def get_num_active_connections(g):
 
 def evolve(g, new_neighbour_fun):
     frozen = False
-    F = len(g.vs.attributes())
+    F = len(g.vs.attributes()) if 'label' not in g.vs.attributes() else len(g.vs.attributes())-1 #rysowanie daje label jako atrybut
     t = 0
     while not frozen:
         t += 1
-        if not (t % 10000):
-            numActiveConnections = get_num_active_connections(g)
-            print("t= ", t, " active: ", numActiveConnections)
-            if numActiveConnections == 0:
-                frozen = True
+        if not (t % 10000): print("t= ", t, " active: ", numActiveConnections)
+        numActiveConnections = get_num_active_connections(g)
+        if numActiveConnections == 0:
+            frozen = True
+
         a = np.random.randint(0, g.vcount())
         if not g.neighbors(a):
             continue
@@ -73,13 +77,13 @@ def get_local_avg_clustering(g):
     return sum(local_list)/len(local_list)
 
 #hiperparametry
-nodesNum = 500
+nodesNum = 20
 n_realizations=2 #po ilu realizacjach dla kazdej wartosc q usredniamy
 
 if __name__ == "__main__":
 
 
-    q_list=[2**q for q in range(2, 500, 1) if 2**q > 250 and 2**q < 1050] # wartosci q dla ktorych symulujemy
+    q_list=[10] # wartosci q dla ktorych symulujemy
     out_simulation_data=dict() # przechowuje wyniki symulacji
     output_path="data"+str(q_list)+".pickle"
 
@@ -100,7 +104,8 @@ if __name__ == "__main__":
 
             evolve(g, new_neighbour_model_a)
 
-            graphs_for_q_list.append(g)
+60
+graphs_for_q_list.append(g)
 
         out_simulation_data[str(q)]=graphs_for_q_list
 
